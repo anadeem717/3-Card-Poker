299a1452d0db45304ba1bf397a78ebc1741d1512
anadeem717 <areeshwork@gmail.com>
2024-11-08 15:55:20 -0600

    Added Card class and Card tests

---
+++ JavaFX_MavenTemplate_VS1 2/src/test/java/CardTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.DisplayName;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+class CardTest {
+
+	@Test
+	void testValidCard() {
+		Card card = new Card('H', 10);
+		assertEquals('H', card.suit);
+		assertEquals(10, card.value);
+	}
+
+	@Test
+	void testInvalidSuit() {
+		Card card = new Card('X', 10);
+		assertEquals('X', card.suit);
+		assertEquals(-1, card.value);
+	}
+
+	@Test
+	void testInvalidValue() {
+		Card card = new Card('H', 20);
+		assertEquals('X', card.suit);
+		assertEquals(-1, card.value);
+	}
+
+	@Test
+	void testInvalidSuitAndValue() {
+		Card card = new Card('Z', 1);
+		assertEquals('X', card.suit);
+		assertEquals(-1, card.value);
+	}
+
+}
+
