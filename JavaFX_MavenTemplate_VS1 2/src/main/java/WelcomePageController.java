3a393fe0acdcd70a2449f230cb913dcca7e81404
anadeem717 <areeshwork@gmail.com>
2024-11-10 03:18:50 -0600

    push card images resources

--- JavaFX_MavenTemplate_VS1 2/src/main/java/WelcomePageController.java
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/WelcomePageController.java
@@ -1,14 +1,12 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.Pane;
-import javafx.scene.control.Button;
 
 public class WelcomePageController implements Initializable {
 
